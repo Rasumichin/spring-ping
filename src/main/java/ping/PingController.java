cfc87506110d81b5df58a7dcffcabe2c1af82309
Erik Lotz <elo@jsmithy.de>
2019-07-16 13:17:46 +0200

    Add a request parameter 'me' with a default value.

--- src/main/java/ping/PingController.java
+++ src/main/java/ping/PingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PingController {
 	
 	@RequestMapping("/ping")
-	public PingEntity ping() {
-		return new PingEntity("default");
-	}
+	public PingEntity ping(@RequestParam(value = "me", defaultValue = "Duke") String meRequestParameter) {
+		return new PingEntity(meRequestParameter);
+	}	
 }
